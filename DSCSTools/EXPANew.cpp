2ed2d4e67156808fd88297fb6c7d8c4259e0b3da
SydMontague <sydmontague@phoenix-staffel.de>
2025-10-02 21:14:50 +0200

    Use proper flags for streams

--- DSCSTools/EXPANew.cpp
+++ DSCSTools/EXPANew.cpp
@@ -38,7 +38,7 @@ namespace
 
         CSVFile(std::filesystem::path path)
         {
-            std::ifstream stream(path);
+            std::ifstream stream(path, std::ios::in);
             aria::csv::CsvParser parser(stream);
 
             for (const auto& row : parser)
@@ -556,7 +556,7 @@ namespace dscstools::expa
         for (const auto& table : file.tables)
         {
             auto path = target / std::format("{:03}_{}.csv", table_id++, table.name);
-            std::ofstream stream(path);
+            std::ofstream stream(path, std::ios::out);
 
             if (!stream) return std::unexpected("Failed to write target file.");
 
